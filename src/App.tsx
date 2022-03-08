19730f10c59bc25a9ec6ccc12f1d730e562e3fa5
ryota-09 <n.ryo950911@gmail.com>
2022-03-08 21:16:26 +0900

    axiosのインストール

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import axios from 'axios';
 import React from 'react';
 
 function App() {
+  const onClickFetchData = async (): Promise<void> => {
+    const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
+    console.log(response.data);
+  }
   return (
     <div className="App">
-      <h1>タイトル</h1>
+      <button onClick={onClickFetchData}>データ取得</button>
     </div>
   );
 }
