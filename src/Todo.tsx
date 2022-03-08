63ba17a934ad8bd57d5c5db4860c029a61495740
ryota-09 <n.ryo950911@gmail.com>
2022-03-08 21:28:22 +0900

    Todo.tsx完成

---
+++ src/Todo.tsx
@@ -0,0 +1,11 @@
+type Props = {
+  userId: number,
+  id: number,
+  title: string,
+  computed: boolean
+}
+
+export const Todo = (props: Props) => {
+  const { title, userId } = props;
+  return <p>{`${title}( ユーザー: ${userId} )`}</p>
+}
