3c29ca45a4715a2100cb3d0b1da2ba22201b9bb7
ryota-09 <n.ryo950911@gmail.com>
2022-03-08 21:23:50 +0900

    繰り返し処理

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,21 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState<any>([]);
+
+
   const onClickFetchData = async (): Promise<void> => {
     const response = await axios.get("https://jsonplaceholder.typicode.com/todos");
     console.log(response.data);
+    setTodoList(response.data);
   }
   return (
     <div className="App">
       <button onClick={onClickFetchData}>データ取得</button>
+      {todoList.map((todo: any) => (
+        <p>{todo.title}</p>
+      ))}
     </div>
   );
 }
